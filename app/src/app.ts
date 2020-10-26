799589fd5a8cf630178454d11f9efcea45ce4cf7
Kaan Kilic <kilickaan1998@gmail.com>
2020-10-26 15:51:41 +0100

    :sparkles: adds dark/light mode logic

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,6 @@
 import { App } from 'jovo-framework';
 import { WebPlatform } from 'jovo-platform-web';
+import { ActionType } from 'jovo-platform-core';
 import { FileDb } from 'jovo-db-filedb';
 import { NlpjsNlu } from 'jovo-nlu-nlpjs';
 
@@ -27,22 +28,26 @@ app.setHandler({
     return this.ask('Hold the microphone button and try to change the background color to blue.');
   },
 
-  ColorIntent() {
-    const color = this.$inputs.color?.key;
+  SwitchThemeIntent() {
+    const theme = this.$inputs.theme?.key;
 
-    if (!color) {
-      return this.ask('I could not understand the color. Try again?');
+    if (theme !== 'dark' && theme !== 'light') {
+      return this.tell('You can switch to either light or dark mode by saying "switch to dark mode" or "switch to light mode".');
     }
 
-    // this.$webApp?.addActions([
-    //   {
-    //     type: 'CUSTOM',
-    //     command: 'set-color',
-    //     value: color,
-    //   },
-    // ]);
-    return this.tell(`OK, ${color}.`);
+    this.$webApp?.addActions([
+      {
+        type: ActionType.Custom,
+        command: 'set-theme',
+        value: theme,
+      },
+    ]);
+    return this.tell(`Alright, I am switching to ${theme} mode.`);
   },
+
+  Unhandled() {
+    return this.tell('You can switch to either light or dark mode by saying "switch to dark mode" or "switch to light mode."');
+  }
 });
 
 export { app };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,6 @@
+const plugin = require("tailwindcss/plugin");
+const selectorParser = require('postcss-selector-parser');
+
 module.exports = {
   future: {
     defaultLineHeights: true,
@@ -19,6 +22,20 @@ module.exports = {
       '2xl': '0 10px 50px -12px rgba(0, 0, 0, 0.25)',
     },
   },
-  variants: {},
-  plugins: [],
-};
+  variants: ['dark'], // TODO: don't enable variant globally (increases file size)
+  plugins: [
+    plugin(function({addVariant, theme, e, prefix}) {
+      const darkSelector = theme('darkSelector', '.mode-dark');
+  
+      addVariant('dark', ({modifySelectors, separator}) => {
+        modifySelectors(({selector}) => {
+          return selectorParser((selectors) => {
+            selectors.walkClasses((sel) => {
+              sel.value = `dark${separator}${sel.value}`;
+              sel.parent.insertBefore(sel, selectorParser().astSync(prefix(`${darkSelector} `)));
+            })
+          }).processSync(selector)
+        })
+      })
+    })
+  ],};
