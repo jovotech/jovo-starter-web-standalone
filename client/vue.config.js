0cbd66e60fde39aab56d50271058bf52ed34a444
aswetlow <a.swetlow@gmail.com>
2020-10-29 10:41:46 +0100

    :recycle: Update standalone starter

--- client/vue.config.js
+++ client/vue.config.js
@@ -3,6 +3,7 @@ module.exports = {
   chainWebpack: (config) => {
     config.resolve.symlinks(false)
   },
+  publicPath: process.env.PUBLICPATH || '',
   css: {
     loaderOptions: {
       postcss: {
